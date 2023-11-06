b67533f94b5faf4dbb0edf917c3cab6752438e5c
ssugita-0914 <148731727+ssugita-0914@users.noreply.github.com>
2023-11-06 10:26:27 +0900

    Add files via upload
    時刻表サイト追加

---
+++ train_schedule/js/kadai.js
@@ -0,0 +1,38 @@
+// This is a JavaScript file
+
+function eki(){
+    var ekiName = document.getElementById("sbox1");
+    var inputName = ekiName.value;
+
+    if(inputName == "博多"||inputName == "博多駅"){
+         window.location.href="hakataeki/hakata_jikoku.html";
+     } else if(inputName == "吉塚"||inputName == "吉塚駅"){
+         window.location.href="yosizuka/yosizuka_jikoku.html";
+
+}else if(inputName == "箱崎"||inputName == "箱崎駅"){
+         window.location.href="hakozaki/hakozaki_jikoku.html";
+
+}else if(inputName == "千早"||inputName == "千早駅"){
+         window.location.href="tihaya/tihaya_jikoku.html";
+
+}else if(inputName == "香椎"||inputName == "香椎駅"){
+         window.location.href="kasii/kasii_jikoku.html";
+
+}else if(inputName == "九産大前"||inputName == "九産大前駅"){
+         window.location.href="kyuusandaimae/kyuusandaimae_jikoku.html";
+
+}else if(inputName == "福工大前"||inputName == "福工大前駅"){
+         window.location.href="fukkoudaimae/fukkoudaimae_jikoku.html";
+
+ }else if(inputName == "新宮中央"||inputName == "新宮中央駅"){
+         window.location.href="singutyuou/singutyuou_jikoku.html";        
+
+}else if(inputName == "竹下"||inputName == "竹下駅"){
+         window.location.href="takesita/takesita_jikoku.html";  
+
+}else if(inputName == "笹原"||inputName == "笹原駅")
+         window.location.href="sasabaru/sasabaru_jikoku.html";      
+
+}
+
+    
